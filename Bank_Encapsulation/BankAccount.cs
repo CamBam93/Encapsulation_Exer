7f134931e585cf548611905ac89be7c8bf710e6a
unknown <cannicam@gmail.com>
2020-11-11 11:49:22 -0600

    finished exercise

---
+++ Bank_Encapsulation/BankAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank_Encapsulation
+{
+    class BankAccount
+    {
+        private double Balance;
+
+        public void Deposit(double amount)
+        {
+            Console.WriteLine($"Depositing {amount} to your account");
+            Balance = amount;
+
+        }
+
+        public double GetBalance()
+        {
+            return Balance;
+        }
+
+    }
+}
---
+++ Bank_Encapsulation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Bank_Encapsulation
+{
+    class Program
+    {
+        //In the main method of your application, create a new instance of the BankAccount class.
+        //Allow the user of the application to Deposit money and retrieve their balance through the console.
+        static void Main(string[] args)
+        {
+            var newAcc = new BankAccount();
+            Console.WriteLine("How many Credits would you like to deposit?" );
+            newAcc.Deposit(double.Parse(Console.ReadLine()));
+            double userBal = newAcc.GetBalance();
+            Console.WriteLine($"Your total Credits are {userBal}");
+        }
+    }
+}
